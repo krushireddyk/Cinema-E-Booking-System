dbb08bbf92059f8df12fc68b9eecc35aeb571c06
Krushi Karukala <krushikarukala@Krushis-MacBook-Air.local>
2022-12-08 18:38:29 -0500

    order history order by bookignID

--- src/main/java/com/cinemaeBooking/repository/BookingRepository.java
+++ src/main/java/com/cinemaeBooking/repository/BookingRepository.java
@@ -11,6 +11,6 @@ import com.cinemaeBooking.entities.Booking;
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Integer>
 {
-    @Query(value = "SELECT * FROM booking b INNER JOIN user u WHERE b.userID = u.userID and u.userName = ?1 order by b.showDate desc, b.showTime desc", nativeQuery = true)
+    @Query(value = "SELECT * FROM booking b INNER JOIN user u WHERE b.userID = u.userID and u.userName = ?1 order by b.bookingID desc", nativeQuery = true)
     public List<Booking> findAllBookingsByUserName(String userName);
 }
